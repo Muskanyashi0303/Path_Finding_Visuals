a9e922afebb24a810e199f28e7dfd499696f7b8c
Jackie Wong <wongjackie888@gmail.com>
2019-10-31 21:38:11 -1000

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import '../stylesheets/App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <nav class='navbar navbar-expand-lg navbar-light bg-light'>
+        <a class='navbar-brand' href='#'>
+          Pathing Visualizer
+        </a>
+        <button
+          class='navbar-toggler'
+          type='button'
+          data-toggle='collapse'
+          data-target='#navbarSupportedContent'
+          aria-controls='navbarSupportedContent'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span class='navbar-toggler-icon'></span>
+        </button>
+
+        <div class='collapse navbar-collapse' id='navbarSupportedContent'>
+          <ul class='navbar-nav mr-auto'>
+            <li class='nav-item active'>
+              <a class='nav-link' href='#'>
+                About <span class='sr-only'>(current)</span>
+              </a>
+            </li>
+            <li class='nav-item'>
+              <a class='nav-link' href='#'>
+                Instructions
+              </a>
+            </li>
+            <li class='nav-item dropdown'>
+              <a
+                class='nav-link dropdown-toggle'
+                href='#'
+                id='navbarDropdown'
+                role='button'
+                data-toggle='dropdown'
+                aria-haspopup='true'
+                aria-expanded='false'
+              >
+                Select Algorithm
+              </a>
+              <div class='dropdown-menu' aria-labelledby='navbarDropdown'>
+                <a class='dropdown-item' href='#'>
+                  Action
+                </a>
+                <a class='dropdown-item' href='#'>
+                  Another action
+                </a>
+                <div class='dropdown-divider'></div>
+                <a class='dropdown-item' href='#'>
+                  Something else here
+                </a>
+              </div>
+            </li>
+            <li class='nav-item'>
+              <a
+                class='nav-link disabled'
+                href='#'
+                tabindex='-1'
+                aria-disabled='true'
+              >
+                Select Mode
+              </a>
+            </li>
+            <li class='nav-item'>
+              <button
+                class='btn btn-outline-success my-2 my-sm-0'
+                type='submit'
+              >
+                Start/Stop
+              </button>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <br />
+      <div>
+        <h3>Pathing Visualizer Area</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
